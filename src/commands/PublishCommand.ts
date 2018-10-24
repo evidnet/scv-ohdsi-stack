39c912751cf0a7d90398028702ca674c42ee6d63
Haze Lee <hazelee@realignist.me>
2018-10-24 10:32:34 +0900

    fix: install script

--- src/commands/PublishCommand.ts
+++ src/commands/PublishCommand.ts
@@ -2,7 +2,7 @@ import { PublishImageCommand, OptionModel } from '@evidnet/scv-core'
 
 export class PublishCommand extends PublishImageCommand {
   rootProject: string = 'base-images'
-  project: string = 'evidnet/r-base'
+  project: string = 'evidnet/ohdsi-stack'
 
   getOptions (): Array<OptionModel> {
     return []
